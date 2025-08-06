436c08c5552927111874b265167696d92b1c69c9
Tamizharasi <tamizharasisrinivasan5@gmail.com>
2025-08-06 14:32:42 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,13 @@
+public class Addition {
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Please enter two command line args as numbers");
+            return;
+        }
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("The sum of" + " "+a+" "+"and"+" "+b+" "+ "is"+" "+(a+b));
+    }
+
+}
